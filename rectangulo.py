af7e9e7031341fc5c8ed3109265aac2aaf22fa66
JosePavas <jpavas887@soyudemedellin.edu.co>
2024-02-20 19:42:43 -0500

    commit 3

---
+++ rectangulo.py
@@ -0,0 +1,16 @@
+#Cree una clase Rectángulo la cual contiene dos atributos de instancia
+#que representan los puntos que definen sus esquinas. 
+#Agregue métodos a la clase Rectángulo para calcular su perímetro, 
+#calcular su área e indicar si el rectángulo es un cuadrado.
+class Rectangulo:
+    
+    def __init__(self, coordenada_x, coordenada_y) -> None:
+        self.coordenada_x = coordenada_x
+        self.coordenada_y = coordenada_y
+        
+    def rectas(self, recta_1, recta_2):
+        self.recta_1 = recta_1
+        self.recta_2 = recta_2
+        
+            
+        
